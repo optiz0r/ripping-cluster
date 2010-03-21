48e596841a2810385726ff502086fea1f655b566
Ben Roberts <ben@sihnon.net>
2010-03-21 01:45:09 +0000

    Fixed inheritence of HBC exception classes

--- HandBrakeCluster/Exceptions.class.php
+++ HandBrakeCluster/Exceptions.class.php
@@ -2,15 +2,15 @@
 
 class HandBrakeCluster_Exception extends Exception {};
 
-class HandBrakeCluster_Exception_DatabaseConfigMissing extends Exception {};
-class HandBrakeCluster_Exception_DatabaseConnectFailed extends Exception {};
-class HandBrakeCluster_Exception_NoDatabaseConnection  extends Exception {};
-class HandBrakeCluster_Exception_DatabaseQueryFailed   extends Exception {};
-class HandBrakeCluster_Exception_ResultCountMismatch   extends Exception {};
+class HandBrakeCluster_Exception_DatabaseConfigMissing extends HandBrakeCluster_Exception {};
+class HandBrakeCluster_Exception_DatabaseConnectFailed extends HandBrakeCluster_Exception {};
+class HandBrakeCluster_Exception_NoDatabaseConnection  extends HandBrakeCluster_Exception {};
+class HandBrakeCluster_Exception_DatabaseQueryFailed   extends HandBrakeCluster_Exception {};
+class HandBrakeCluster_Exception_ResultCountMismatch   extends HandBrakeCluster_Exception {};
 
-class HandBrakeCluster_Exception_UnknownSetting        extends Exception {};
+class HandBrakeCluster_Exception_UnknownSetting        extends HandBrakeCluster_Exception {};
 
-class HandBrakeCluster_Exception_TemplateException     extends Exception {};
+class HandBrakeCluster_Exception_TemplateException     extends HandBrakeCluster_Exception {};
 class HandBrakeCluster_Exception_Unauthorized          extends HandBrakeCluster_Exception_TemplateException {};
 class HandBrakeCluster_Exception_FileNotFound          extends HandBrakeCluster_Exception_TemplateException {};
 
