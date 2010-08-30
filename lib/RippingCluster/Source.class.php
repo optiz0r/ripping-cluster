22613e030e4f64a029895c1873e3deb5ff08127a
Ben Roberts <ben@sihnon.net>
2010-08-30 22:03:49 +0100

    Adding Source class from earlier botched commit.

---
+++ lib/RippingCluster/Source.class.php
@@ -0,0 +1,105 @@
+<?php
+
+class RippingCluster_Source {
+    
+    const PM_TITLE = 0;
+    const PM_CHAPTER = 1;
+    const PM_AUDIO = 2;
+    const PM_SUBTITLE = 3;
+    
+    protected $filename;
+    protected $plugin;
+    protected $titles = array();
+
+    public function __construct($source_filename, $plugin) {
+        $this->filename = $source_filename;
+        $this->plugin = $plugin;
+
+    }
+    
+    public static function isCached($source_filename) {
+        $main   = RippingCluster_Main::instance();
+        $cache  = $main->cache();
+        $config = $main->config();
+
+        return $cache->exists($source_filename, $config->get('rips.cache_ttl'));
+    }
+    
+    public function cache() {
+        $main   = RippingCluster_Main::instance();
+        $cache  = $main->cache();
+        $config = $main->config();
+        
+        $cache->store($this->filename, serialize($this), $config->get('rips.cache_ttl'));
+    }
+    
+    public static function encodeFilename($filename) {
+        return str_replace("/", "-", base64_encode($filename));
+    }
+    
+    public function addTitle(RippingCluster_Rips_SourceTitle $title) {
+        $this->titles[] = $title;
+    }
+    
+	public function longestTitle() {
+	    $longest_title = null;
+	    $maximum_duration = 0;
+	    
+	    if ( ! $this->titles) {
+	        return null;
+	    }
+	    
+	    foreach ($this->titles as $title) {
+	        $duration = $title->durationInSeconds();
+	        if ($duration > $maximum_duration) {
+	            $longest_title = $title;
+	            $maximum_duration = $duration;
+	        }
+	    }
+	    
+	    return $longest_title;
+    }
+
+    public function longestTitleIndex() {
+        $longest_index = null;
+        $maximmum_duration = 0;
+
+        if ( ! $this->titles) {
+            return null;
+        }
+
+        for ($i = 0, $l = count($this->titles); $i < $l; ++$i) {
+            $title = $this->titles[$i];
+            $duration = $title->durationInSeconds();
+            if ($duration > $maximum_duration) {
+                $longest_index = $i;
+                $maximum_duration = $duration;
+            }
+        }
+
+        return $longest_index;
+    }
+	
+    public function filename() {
+        return $this->filename;
+    }
+    
+    public function filenameEncoded() {
+        return self::encodeFilename($this->filename);
+    }
+    
+    public function plugin() {
+        return $this->plugin;
+    }
+
+    public function titleCount() {
+        return count($this->titles);
+    }
+
+    public function titles() {
+        return $this->titles;
+    }
+
+};
+
+?>
