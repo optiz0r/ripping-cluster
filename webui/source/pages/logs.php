9ec2df1f183da67dcf12d03e046de56377d14e7b
Ben Roberts <ben@sihnon.net>
2011-04-24 11:10:07 +0100

    Bug fix: incorrect parameters to log retrieval calls

--- webui/source/pages/logs.php
+++ webui/source/pages/logs.php
@@ -2,8 +2,8 @@
 
 $log = RippingCluster_Main::instance()->log();
 
-$client_log_entries = RippingCluster_LogEntry::recentEntries($log, 'webui', 30);
-$worker_log_entries = RippingCluster_LogEntry::recentEntries($log, 'worker', 30);
+$client_log_entries = RippingCluster_LogEntry::recentEntries($log, 'webui', 'ctime', SihnonFramework_Log::ORDER_DESC, 30);
+$worker_log_entries = RippingCluster_LogEntry::recentEntries($log, 'worker', 'ctime', SihnonFramework_Log::ORDER_DESC, 30);
 
 $this->smarty->assign('client_log_entries', $client_log_entries);
 $this->smarty->assign('worker_log_entries', $worker_log_entries);
