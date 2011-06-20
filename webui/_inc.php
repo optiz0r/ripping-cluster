efb7db35d8f0f29fc0f15bee95544dd487d410f9
Ben Roberts <ben@sihnon.net>
2011-06-20 22:33:18 +0100

    Fixed typo in _inc.php

--- webui/_inc.php
+++ webui/_inc.php
@@ -3,7 +3,7 @@
 require_once '/etc/ripping-cluster/config.php';
 require_once SihnonFramework_Lib . 'SihnonFramework/Main.class.php';
 
-SihnonFramework::registerAutoloadClasses('SihnonFramework', SihnonFramework_Lib,
+SihnonFramework_Main::registerAutoloadClasses('SihnonFramework', SihnonFramework_Lib,
 												'RippingCluster', RippingCluster_Lib);
 
 ?>
