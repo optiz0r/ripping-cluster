841a5b9f924abe769f9531ad6bef07980eabe923
Ben Roberts <ben@sihnon.net>
2011-06-20 22:04:44 +0100

    Revert previous untested change in include statements

--- webui/_inc.php
+++ webui/_inc.php
@@ -1,9 +1,9 @@
 <?php
 
 require_once '/etc/ripping-cluster/config.php';
-require_once RippingCluster_Lib . 'RippingCluster/Main.class.php';
+require_once SihnonFramework_Lib . 'SihnonFramework/Main.class.php';
 
-RippingCluster_Main::registerAutoloadClasses('SihnonFramework', SihnonFramework_Lib,
+SihnonFramework::registerAutoloadClasses('SihnonFramework', SihnonFramework_Lib,
 												'RippingCluster', RippingCluster_Lib);
 
 ?>
