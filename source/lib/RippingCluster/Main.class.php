b93efc9878e6d81bf1be8eacf3d60f063bda1849
Ben Roberts <ben@sihnon.net>
2011-08-06 14:55:29 +0100

    Replace if block with switch case for extensibility

--- source/lib/RippingCluster/Main.class.php
+++ source/lib/RippingCluster/Main.class.php
@@ -11,24 +11,27 @@ class RippingCluster_Main extends SihnonFramework_Main {
 
     protected function __construct() {
         parent::__construct();
-        
-        $request_string = isset($_GET['l']) ? $_GET['l'] : '';
-        
-        $this->request  = new RippingCluster_RequestParser($request_string);
-        
-        if (HBC_File == 'index') {
-            $this->smarty = new Smarty();
-            $this->smarty->template_dir = './source/templates';
-            $this->smarty->compile_dir  = './tmp/templates';
-            $this->smarty->cache_dir    = './tmp/cache';
-            $this->smarty->config_dir   = './config';
-    
-            $this->smarty->registerPlugin('modifier', 'formatDuration', array('RippingCluster_Main', 'formatDuration'));
 
-            $this->smarty->assign('version', '0.1');
-            $this->smarty->assign('messages', array());
-        
-            $this->smarty->assign('base_uri', $this->base_uri);
+        $request_string = isset($_GET['l']) ? $_GET['l'] : '';
+
+        $this->request  = new RippingCluster_RequestParser($request_string);
+
+        switch (HBC_File) {
+            case 'index': {
+                $this->smarty = new Smarty();
+                $this->smarty->template_dir = './source/templates';
+                $this->smarty->compile_dir  = './tmp/templates';
+                $this->smarty->cache_dir    = './tmp/cache';
+                $this->smarty->config_dir   = './config';
+                 
+                $this->smarty->registerPlugin('modifier', 'formatDuration', array('RippingCluster_Main', 'formatDuration'));
+
+                $this->smarty->assign('version', '0.1');
+                $this->smarty->assign('messages', array());
+                 
+                $this->smarty->assign('base_uri', $this->base_uri);
+            } break;
+
         }
     }
 
@@ -37,14 +40,14 @@ class RippingCluster_Main extends SihnonFramework_Main {
     }
 
     /**
-     * 
+     *
      * @return RippingCluster_RequestParser
      */
     public function request() {
         return $this->request;
     }
-    
-    
+
+
 }
 
 ?>
