4818193d37995ec1393153fab092269ca99d5c83
Ben Roberts <ben@sihnon.net>
2011-08-29 19:22:56 +0100

    Sort admin settings by name to make searching easier

--- webui/source/pages/admin/settings.php
+++ webui/source/pages/admin/settings.php
@@ -4,6 +4,7 @@ $main   = RippingCluster_Main::instance();
 $config = $main->config();
 
 $settings = $config->enumerateAll();
+asort($settings);
 $this->smarty->assign('settings', $settings);
 $this->smarty->assign('config', $config);
 
