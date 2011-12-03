06e3d65ffa0d3303257bc3db8933f984eb8c2f09
Ben Roberts <ben@sihnon.net>
2011-12-03 19:47:57 +0000

    Remove nested tmp directory

--- source/lib/RippingCluster/Main.class.php
+++ source/lib/RippingCluster/Main.class.php
@@ -22,8 +22,8 @@ class RippingCluster_Main extends SihnonFramework_Main {
                 $smarty_tmp = '/var/tmp/ripping-cluster';
                 $this->smarty = new Smarty();
                 $this->smarty->template_dir = static::makeAbsolutePath('./source/templates');
-                $this->smarty->compile_dir  = static::makeAbsolutePath($smarty_tmp . '/tmp/templates');
-                $this->smarty->cache_dir    = static::makeAbsolutePath($smarty_tmp . '/tmp/cache');
+                $this->smarty->compile_dir  = static::makeAbsolutePath($smarty_tmp . '/templates');
+                $this->smarty->cache_dir    = static::makeAbsolutePath($smarty_tmp . '/cache');
                 $this->smarty->config_dir   = static::makeAbsolutePath($smarty_tmp . '/config');
                 $this->smarty->plugins_dir[]= static::makeAbsolutePath('./source/smarty/plugins');
                  
